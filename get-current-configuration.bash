c5104ba7b123d76280f324625a3638a29e46652b
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2019-01-02 09:32:09 +0000

    Backup xfce4 panel configuration

--- get-current-configuration.bash
+++ get-current-configuration.bash
@@ -10,6 +10,9 @@ cp -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml $DOTFIL
 cp -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml $DOTFILES/system/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 cp -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml $DOTFILES/system/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml
 cp -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml $DOTFILES/system/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml
+cp -f $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml $DOTFILES/system/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 cp -f $HOME/.config/xfce4/terminal/accels.scm $DOTFILES/system/xfce4/terminal/accels.scm
 cp -f $HOME/.config/xfce4/terminal/terminalrc $DOTFILES/system/xfce4/terminal/terminalrc
+cp -f $HOME/.config/xfce4/panel/cpugraph-8.rc $DOTFILES/system/xfce4/panel/cpugraph-8.rc
+cp -f $HOME/.config/xfce4/panel/default.xml $DOTFILES/system/xfce4/panel/default.xml
 
