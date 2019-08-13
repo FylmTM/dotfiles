be3f614ba96406cb197ec18fccbc3d8452294af6
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2019-08-13 11:39:54 +0300

    Fish kubectl completions

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -9,6 +9,7 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
     fisher add matchai/spacefish
+    fisher add evanlucas/fish-kubectl-completions
 end
 
 set fish_greeting
